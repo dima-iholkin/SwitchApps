5497be083cd3d838653aaac35e31c494e7f96af5
Dima Iholkin <38999978+dima-iholkin@users.noreply.github.com>
2020-07-07 02:31:50 +0300

    WIP on the installer.

---
+++ installer/wix-installer/install-scheduler-task.ps1
@@ -0,0 +1,51 @@
+function Install {
+  [CmdletBinding()]
+  param (
+      [Parameter()]
+      [String]
+      $InstallDir
+  )
+  # Write-Output HelloWorld;
+  # Read-Host;
+
+
+
+  try {
+    Unregister-ScheduledTask -TaskPath "\SwitchApps\" -TaskName "SwitchApps autostart" -Confirm:$false -ErrorAction SilentlyContinue
+  }
+  catch {
+    Write-Output "Unable to delete the task."
+  }
+  # Write-Output "Continue..."
+
+
+
+  # $A = New-ScheduledTaskAction -Execute "C:\MySoftware\Projects\SwitchApps\installer\assets\SwitchApps.exe"
+  $PathToExe = $InstallDir + "\SwitchApps.exe"
+  $A = New-ScheduledTaskAction -Execute $PathToExe
+
+
+
+  $user = [System.Security.Principal.WindowsIdentity]::GetCurrent();
+
+  $P = New-ScheduledTaskPrincipal -UserId $user.User.Value -RunLevel Highest
+
+  $T = New-ScheduledTaskTrigger -AtLogon -User $user.Name
+
+
+
+  $S = New-ScheduledTaskSettingsSet -Priority 0 -AllowStartIfOnBatteries
+  $S.ExecutionTimeLimit = 'PT0S';
+
+
+
+  $D = New-ScheduledTask -Action $A -Principal $P -Trigger $T -Settings $S
+
+
+
+  Register-ScheduledTask -TaskPath "SwitchApps" -TaskName "SwitchApps autostart" -InputObject $D
+}
+
+
+
+Install;
