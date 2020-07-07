85b6f1dbf1fd213e1b29a682d9a9d535728566a6
Dima Iholkin <38999978+dima-iholkin@users.noreply.github.com>
2020-07-07 23:25:21 +0300

    Add the Uninstall logic.

--- installer/wix-installer/install-scheduler-task.ps1
+++ installer/wix-installer/install-scheduler-task.ps1
@@ -1,9 +1,9 @@
 function Install {
   [CmdletBinding()]
   param (
-      [Parameter()]
-      [String]
-      $InstallDir
+    [Parameter()]
+    [String]
+    $InstallDir
   )
   Write-Output "Install started";
 
@@ -40,17 +40,70 @@ function Install {
 
   $D = New-ScheduledTask -Action $A -Principal $P -Trigger $T -Settings $S
 
-
-
   Register-ScheduledTask -TaskPath "SwitchApps" -TaskName "SwitchApps autostart" -InputObject $D
+  Write-Output "Scheduler task created."
+
+
+
+  # $thumbSize = Get-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Taskband" -Name MinThumbSizePx
+  # Set-ItemProperty -Path "HKCU:\Software\SwitchApps\Backup" -Name ThumbnailPreviewSize_Backup -Value $thumbSize.MinThumbSizePx
+
+  # $thumbDelay = Get-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name ExtendedUIHoverTime
+  # Set-ItemProperty -Path "HKCU:\Software\SwitchApps\Backup" -Name ThumbnailPreviewDelay_Backup -Value $thumbDelay.ExtendedUIHoverTime
+
+  # if (Test-Path -Path "HKCU:\Software\Classes\ms-officeapp\Shell\Open\Command") {
+  #   $val = Get-ItemProperty -Path "HKCU:\Software\Classes\ms-officeapp\Shell\Open\Command"
+  #   Set-ItemProperty -Path "HKCU:\Software\SwitchApps\Backup" -Name MsOfficeAdPopup_Backup -Value $val.'(default)'
+  # } else {
+  #   Set-ItemProperty -Path "HKCU:\Software\SwitchApps\Backup" -Name MsOfficeAdPopup_Backup -Value "false"
+  # }
+  
+  # Write-Output "Registry backup created."
 }
 
 
 
 function Uninstall {
-  Write-Output "Uninstall started";
+  [CmdletBinding()]
+  param (
+    [Parameter()]
+    [string] $ThumbSize,
+    [Parameter()]
+    [string] $ThumbDelay,
+    [Parameter()]
+    [string] $MsOfficePopup
+  )
+  Write-Output "Uninstall started...";
 
+  $str = "ThumbSize: " + $ThumbSize
+  $str1 = "ThumbDelay: " + $ThumbDelay
+  $str2 = "MsOfficePopup: " + $MsOfficePopup
+  Write-Output $str
+  Write-Output $str1
+  Write-Output $str2
+  Start-Sleep -Seconds 5
+
+
+  
   Unregister-ScheduledTask -TaskPath "\SwitchApps\" -TaskName "SwitchApps autostart" -Confirm:$false  
+
+  Write-Output "Scheduler task uninstalled."
+
+
+
+  $thumbSizeInt = $ThumbSize.Remove(0, 3) -as [int]
+  Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Taskband" -Name MinThumbSizePx -Value $thumbSizeInt
+
+  $thumbDelayInt = $ThumbDelay.Remove(0, 3) -as [int]
+  Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name ExtendedUIHoverTime -Value $thumbDelayInt
+
+  if ($MsOfficePopup -eq "false") {
+    Remove-Item -Path "HKCU:\Software\Classes\ms-officeapp\Shell\Open\Command"
+  } else {
+    Set-ItemProperty -Path "HKCU:\Software\Classes\ms-officeapp\Shell\Open\Command"  -Value $MsOfficePopup
+  }
+
+  Write-Output "Registry changes reverted."
 }
 
 
