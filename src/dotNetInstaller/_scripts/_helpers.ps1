9ef8729a62430db86ebb4df743704c49c6c1b372
Dima Iholkin <38999978+dima-iholkin@users.noreply.github.com>
2021-11-15 06:27:13 +0200

    Create the build script.

--- src/dotNetInstaller/_scripts/_helpers.ps1
+++ src/dotNetInstaller/_scripts/_helpers.ps1
@@ -12,6 +12,17 @@
 
 
 
+$ErrorActionPreference = "Stop"
+
+
+
+function DebugMe {
+  dir
+}
+
+
+
+
 enum Platform {
   x64
   x86
@@ -43,7 +54,7 @@ function BuildExeAndInstaller {
     [Mod] $Mod
   )
   BuildExe -Platform $Platform -Mod $Mod
-  BuildInstaller -Mod
+  BuildInstaller -Platform $Platform -Mod $Mod
 }
 
 
@@ -60,8 +71,8 @@ function BuildExe {
   
   $binPlatform
   switch ($Platform) {
-    Platform.x64 { $binPlatform = "\Unicode 64-bit.bin" }
-    Platform.x86 { $binPlatform = "\Unicode 32-bit.bin" }
+    x64 { $binPlatform = "\Unicode 64-bit.bin" }
+    x86 { $binPlatform = "\Unicode 32-bit.bin" }
     Default { throw "Unexpected platform argument." }
   }
   $binFile = $ahkCompilerPath + $binPlatform
@@ -73,14 +84,13 @@ function BuildExe {
   $rootDir = Split-Path -Path $installerDir -Parent # \src\
 
   $ahkSourceFile = $rootDir + "\SwitchApps.ahk"
-  Copy-Item -Path $ahkSourceFile -Destination $buildDir
+  Copy-Item -Path $ahkSourceFile -Destination $buildDir -Force
   $copiedAhkFile = $buildDir + "\SwitchApps.ahk"
 
-  # $binPlatform
   switch ($Mod) {
-    Mod.Normal { }
-    Mod.NoUngroupMod { 
-      (Get-Content -path $copiedAhkFile -Raw) -replace 'Send {Enter}','Send {Up} Send {Enter}'
+    Normal { }
+    NoUngroupMod { 
+      (Get-Content -path $copiedAhkFile -Raw) -replace 'Send {Enter}', 'Send {Up} Send {Enter}' | Set-Content -Path $copiedAhkFile
     }
     Default { throw "Unexpected mod argument." }
   }
@@ -99,24 +109,55 @@ function BuildInstaller {
   [CmdletBinding()]
   param (
     [Parameter()]
-    [Platform] $Platform
+    [Platform] $Platform,
+    [Mod] $Mod
   )
   $scriptsDir = (Get-Location).Path # \src\dotNetInstaller\_scripts
   $installerDir = Split-Path -Path $scriptsDir -Parent # \src\dotNetInstaller
-  # $projectFile = $installerDir + "\SwitchApps_Installer\SwitchApps_Installer.vdproj"
+  $projectFile = $installerDir + "\SwitchApps_Installer\SwitchApps_Installer.vdproj" # \src\dotNetInstaller\SwitchApps_Installer\SwitchApps_Installer.vdproj
   $solutionFile = $installerDir + "\SwitchApps.sln"
 
   switch ($Platform) {
-    Platform.x64 { }
-    Platform.x86 {
-      (Get-Content -path $projectFile -Raw) -replace '"TargetPlatform" = "3:1"','"TargetPlatform" = "3:0"'
+    x64 { }
+    x86 {
+      (Get-Content -path $projectFile -Raw) -replace '"TargetPlatform" = "3:1"', '"TargetPlatform" = "3:0"' | Set-Content -Path $projectFile
+      (Get-Content -path $projectFile -Raw) -replace 'ProgramFiles64Folder', 'ProgramFilesFolder' | Set-Content -Path $projectFile
     }
     Default { throw "Unexpected platform argument." }
   }
 
   $devenvFile = "C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\IDE\devenv.exe"
-  & $devenvFile $solutionFile /rebuild Debug /project "SwitchApps_Installer\SwitchApps_Installer.vdproj"
+  Write-Output "devenv.exe started."
+  $logFile = $installerDir + "\SwitchApps_Installer\Debug\log.txt"
+  Start-Process -FilePath $devenvFile -ArgumentList ($solutionFile + " /rebuild Debug /Out " + $logFile) -Wait
+  Write-Output "devenv.exe finished."
 
+  switch ($Platform) {
+    x64 { }
+    x86 {
+      (Get-Content -path $projectFile -Raw) -replace '"TargetPlatform" = "3:0"', '"TargetPlatform" = "3:1"' | Set-Content -Path $projectFile
+      (Get-Content -path $projectFile -Raw) -replace 'ProgramFilesFolder', 'ProgramFiles64Folder' | Set-Content -Path $projectFile
+    }
+    Default { throw "Unexpected platform argument." }
+  }
+
+  $installerFile = $installerDir + "\SwitchApps_Installer\Debug\SwitchApps.msi" # \src\dotNetInstaller\SwitchApps_Installer\Debug\SwitchApps.msi
+  $buildDir = $installerDir + "\_build" # \src\dotNetInstaller\_build
+
+  $installerPlatform
+  switch ($Platform) {
+    x64 { $installerPlatform = "_x64" }
+    x86 { $installerPlatform = "_x86" }
+    Default { throw "Unexpected platform argument." }
+  }
+  $installerMod
+  switch ($Mod) {
+    Normal { $installerMod = "" }
+    NoUngroupMod { $installerMod = "_NoUngroupMod" }
+    Default { throw "Unexpected mod argument." }
+  }
+
+  Copy-Item -Path $installerFile -Destination ($buildDir + "\SwitchApps" + $installerPlatform + $installerMod + ".msi") -Force
 }
 
 
