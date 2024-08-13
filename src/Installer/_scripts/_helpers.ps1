92e5187b316dab8cce218ce4b4cb484c79cdf4ca
Dima Iholkin <38999978+dima-iholkin@users.noreply.github.com>
2024-08-13 16:13:12 +0300

    Try a fix.

--- src/Installer/_scripts/_helpers.ps1
+++ src/Installer/_scripts/_helpers.ps1
@@ -147,8 +147,8 @@ function BuildInstaller {
   Write-Output "devenv.exe started: platform $Platform, mod $Mod."
   # $toLogOrNot = "/Out " + $installerDir + "\SwitchApps_Installer\Debug\log.txt"
   $toLogOrNot = ""
-  $proc = Start-Process -FilePath $devenvFile -ArgumentList ("$solutionFile /Rebuild Debug") -NoNewWindow -PassThru
   $timeoutReached = $null
+  $proc = Start-Process -FilePath $devenvFile -ArgumentList ("$solutionFile /Rebuild Debug") -NoNewWindow -PassThru
   $proc | Wait-Process -Timeout 60 -ErrorAction SilentlyContinue -ErrorVariable timeoutReached
   if ($timeoutReached) {
     # Terminate the process:
