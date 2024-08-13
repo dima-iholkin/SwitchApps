2605917d776531c258922f6e0fc0874adb927c46
Dima Iholkin <38999978+dima-iholkin@users.noreply.github.com>
2024-08-13 11:22:25 +0300

    Try a fix.

--- src/Installer/_scripts/_helpers.ps1
+++ src/Installer/_scripts/_helpers.ps1
@@ -147,7 +147,7 @@ function BuildInstaller {
   Write-Output "devenv.exe started: platform $Platform, mod $Mod."
   # $toLogOrNot = "/Out " + $installerDir + "\SwitchApps_Installer\Debug\log.txt"
   $toLogOrNot = ""
-  Start-Process -FilePath $devenvFile -ArgumentList ($solutionFile + " /rebuild Debug $toLogOrNot") -Wait | Wait-Process -Timeout 15
+  Start-Process -FilePath $devenvFile -ArgumentList ("/runexit " + $solutionFile + " /rebuild Debug $toLogOrNot") -Wait
   Write-Output "devenv.exe finished: platform $Platform, mod $Mod."
   # Revert the project file's modification after an x86 platform run:
   switch ($Platform) {
